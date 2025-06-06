324041db7004c408527c4d28d4531c98d8cb97ee
Hanabri <g313756670117@outlook.com>
2025-06-06 13:53:58 +0800

    feature:6.6

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: 'jack-he-1.github.io/baoshi-square',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
